1c8dcb6bf417a83b53acd9485fc8b1ee0302269f
Byeong Min Cho <merrybmc@gmail.com>
2024-04-29 16:33:46 +0900

    Math Object

---
+++ javascript/Math Object/script.js
@@ -0,0 +1,9 @@
+val = Math.E; // Eular
+val = Math.PI; // PI
+val = Math.round(2.4); // 2 가장 가까운 정수로 내림/올림
+val = Math.ceil(2.4); // 3 무조건 올림
+val = Math.floor(2.4); // 2 무조건 내림
+val = Math.abs(-5); // 절대값
+val = Math.min(1, 2, 3, 4, 5); // 가장 작은 값
+val = Math.max(1, 2, 3, 4, 5); // 가장 큰 값
+val = Math.random() * 20 + 1; // 1~20의 값 랜덤
